549e6d7b83f753cc9ab1a5f0bfad7428662b3c71
Uchio Kondo <udzura@udzura.jp>
2026-09-21 13:15:02 +0900

    Fix worker-wasm mgem dep

--- covers/backends/worker/build_config/picoruby-worker-wasm.rb
+++ covers/backends/worker/build_config/picoruby-worker-wasm.rb
@@ -26,18 +26,7 @@ MRuby::CrossBuild.new("picoruby-worker-wasm") do |conf|
     conf.gem github: "udzura/picoruby-sinatra-covers", branch: "master"
   end
 
-  # Register Sinatra's dependency sources first, then optional local overrides.
-  %w[MRUBY_MUSTERMANN_GEM_DIR MRUBY_RACK_GEM_DIR].each do |environment|
-    if (gem_dir = ENV[environment])
-      conf.gem gemdir: File.expand_path(gem_dir)
-    end
-  end
-
-  if (gem_dir = ENV["PICORUBY_WORKER_WASM_GEM_DIR"])
-    conf.gem gemdir: File.expand_path(gem_dir)
-  else
-    conf.gem github: "udzura/picoruby-cloudflare-worker-wasm",
-             branch: "master",
-             checksum_hash: "b342a9a1952332cc8fa0e1c41eb4e458c3a8b0a9"
-  end
+  conf.gem github: "udzura/picoruby-cloudflare-worker-wasm",
+            branch: "master",
+            checksum_hash: "ebab3afc4b06cdb29508de453795de90349c4691"
 end
